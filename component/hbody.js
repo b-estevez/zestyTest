ea5a836c8cf1d45b0c93a3ac13997a7092252050
Bry <Google-Ads-Repository>
2022-10-11 01:05:36 +0800

    commit

--- component/hbody.js
+++ component/hbody.js
@@ -1,19 +1,25 @@
 import parse from "html-react-parser";
 export default function Hbody({ hcontent }) {
   return (
-    <>
+  <>
       {hcontent.map((items, index) => {
-        
-        return (<div className="card">
+        return (
+          <div className="card">
             <h1>{items.title}</h1>
-          <div key={index} className="card-body">
-            {items.text_content === null || items.text_content === ""
-              ? parse("<h3 className='text-danger'> No information Available</h3>")
-              : parse(items.text_content)}
-          </div>
+            <div key={index} className="card-body">
+              {items.text_content === null || items.text_content === "" ? (
+                parse(
+                  "<h3 className='text-danger'> No information Available</h3>"
+                )
+              ) : (
+                <div dangerouslySetInnerHTML={{ __html: items.text_content }} />
+              )}
+            </div>
           </div>
         );
       })}
+
+ 
     </>
   );
 }
